7f3469459616fa1a38fc1913cf0538958c32afa1
AbdullahElsayed10 <abdukha141@gmail.com>
2025-09-17 13:56:25 +0300

    DS Client

--- Project__Bank__Route/Client.h
+++ Project__Bank__Route/Client.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <victor>
+#include<fstream>
 using namespace std;
 
 class Client {
@@ -62,3 +64,64 @@ public:
         cout << "Balance: " << balance << endl;
     }
 };
+// -------------------- Parser --------------------
+class Parser {
+public:
+    static vector<string> split(string line) {
+        vector<string> tokens;
+        string token;
+        for (char c: line) {
+            if (c == '|') {
+                tokens.push_back(token);
+                token.clear();
+            } else {
+                token.push_back(c);
+            }
+        }
+        tokens.push_back(token);
+        return tokens;
+    }
+
+    static Client parseToClient(string line) {
+        vector<string> parts = split(line);
+        Client c;
+        c.setId(stoi(parts[0]));
+        c.setName(parts[1]);
+        c.setPassword(parts[2]);
+        c.setBalance(stod(parts[3]));
+        return c;
+    }
+};
+
+// -------------------- FilesHelper --------------------
+class FilesHelper {
+public:
+    static void saveClient(Client c) {
+        ofstream file("Clients.txt", ios::app);
+        if (file.is_open()) {
+            file << c.getId() << "|"
+                    << c.getName() << "|"
+                    << c.getPassword() << "|"
+                    << c.getBalance() << "" << endl;
+            file.close();
+        }
+    }
+
+    static vector<Client> getClients() {
+        vector<Client> clients;
+        ifstream file("Clients.txt");
+        string line;
+        while (getline(file, line)) {
+            Client c = Parser::parseToClient(line);
+            clients.push_back(c);
+        }
+        file.close();
+        return clients;
+    }
+
+    static void clearClients() {
+        ofstream file("Clients.txt", ios::trunc);
+        file.close();
+    }
+};
+
